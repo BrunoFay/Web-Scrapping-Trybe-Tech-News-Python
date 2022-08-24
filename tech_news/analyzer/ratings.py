6f4602c74e8573c16d5950f7240510b6fce3ff56
Bruno <brunofay1@hotmail.com>
2022-08-24 15:10:36 -0300

    10 feita

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,4 +1,4 @@
-from tech_news.database import find_top_5_news
+from tech_news.database import find_top_5_news, find_news
 
 
 # Requisito 10
@@ -12,3 +12,10 @@ def top_5_news():
 # Requisito 11
 def top_5_categories():
     """Seu código deve vir aqui"""
+    count_categories = {}
+    categories_sorted = find_news()
+    categories_list = [category['category'] for category in categories_sorted]
+    for category in categories_list:
+        count_categories[category] = count_categories.get(category, 0) + 1
+
+    return sorted(count_categories, key=lambda x: (-count_categories[x], x))
