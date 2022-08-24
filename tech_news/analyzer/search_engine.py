ab2fa6faa7487f8e80c2da9831603e750dc9ca8d
Bruno <brunofay1@hotmail.com>
2022-08-24 02:38:58 -0300

    7 feita

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import search_news
+from datetime import datetime
 
 
 # Requisito 6
@@ -6,10 +7,20 @@ def search_by_title(title):
     """Seu código deve vir aqui"""
     query = {'title': {"$regex": title, "$options": "i"}}
     result = search_news(query)
+    return [(new['title'], new['url']) for new in result]
+
 
 # Requisito 7
+
 def search_by_date(date):
     """Seu código deve vir aqui"""
+    try:
+        date_formatted_br = datetime.fromisoformat(date).strftime("%d/%m/%Y")
+        query = {'timestamp': date_formatted_br}
+        result = search_news(query)
+        return [(new['title'], new['url']) for new in result]
+    except ValueError:
+        raise ValueError('Data inválida')
 
 
 # Requisito 8
