a337a2f7158648d21fecb92f2369dab69c28e5d4
Bruno <brunofay1@hotmail.com>
2022-08-24 02:50:47 -0300

    9 feita

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -34,3 +34,6 @@ def search_by_tag(tag):
 # Requisito 9
 def search_by_category(category):
     """Seu código deve vir aqui"""
+    query = {'category': {"$regex": category, "$options": 'i'}}
+    result = search_news(query)
+    return [(new['title'], new['url']) for new in result]
